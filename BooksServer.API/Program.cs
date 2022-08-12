029d91f86332ab0fc295eb88687e0b087c0d72d7
unknown <mahmood.hussein.basha@gmail.com>
2022-08-12 17:33:51 +0300

    Works without queue

--- BooksServer.API/Program.cs
+++ BooksServer.API/Program.cs
@@ -1,6 +1,7 @@
 using BooksServer.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using BooksServer.API.Repository;
+using Refit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,9 @@ builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddDbContext<BooksDbContexst>(options => 
 options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<BooksRepository>();
+
+builder.Services.AddRefitClient<IBookSyncRepository>().ConfigureHttpClient(c =>
+c.BaseAddress = new Uri(builder.Configuration.GetConnectionString("BookSyncAPI")));
 
 var app = builder.Build();
 
